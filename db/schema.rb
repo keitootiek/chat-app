904581c350cd8cf9637d23e2d8b67345596d798d
keitootiek <137137801+keitootiek@users.noreply.github.com>
2023-06-27 16:11:54 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_054355) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
